3c0cba3018954011bccf0d1f58ffdd5d91e49342
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2022-09-28 18:44:50 +0200

    Added a bit more documentation to Calendar.java

--- src/main/java/com/mystic_alchemy/ics2java/calendar/Calendar.java
+++ src/main/java/com/mystic_alchemy/ics2java/calendar/Calendar.java
@@ -3,6 +3,9 @@ package com.mystic_alchemy.ics2java.calendar;
 import java.util.ArrayList;
 
 /**
+ * Represents an iCal calendar with its events.
+ * Doesn't handle weekdays on its own.
+ *
  * @author PilleniusMC for Mystic-Alchemy
  * @since 0.1
  * @version 0.1
