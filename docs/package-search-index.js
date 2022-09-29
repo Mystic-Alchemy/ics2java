2fe797fdddaeb57f24762b3047fc5c5b7d04da72
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2022-09-29 16:21:21 +0200

    Rebuilding docs due to failure.

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mystic_alchemy.ics2java"},{"l":"com.mystic_alchemy.ics2java.calendar"},{"l":"com.mystic_alchemy.ics2java.enums"}];updateSearchResults();
--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.mystic_alchemy.ics2java.calendar","l":"Calendar"},{"p":"com.mystic_alchemy.ics2java","l":"CalendarParser"},{"p":"com.mystic_alchemy.ics2java.calendar","l":"Event"},{"p":"com.mystic_alchemy.ics2java.enums","l":"RecurrenceFrequency"}];updateSearchResults();
