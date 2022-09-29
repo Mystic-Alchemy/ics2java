63fbaa973b58d88fe9c0efd6282c4cd3c969c3a9
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2022-09-29 16:19:49 +0200

    Removing the module info again... not necessary

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mystic_alchem.ics2java {
-    requires commons.io;
-    requires org.jetbrains.annotations;
-    exports com.mystic_alchemy.ics2java;
-    exports com.mystic_alchemy.ics2java.calendar;
-    exports com.mystic_alchemy.ics2java.enums;
-}
