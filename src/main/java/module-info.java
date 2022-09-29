e493eada931d8279cfed89f0a6f4bce69f59dc22
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2022-09-29 16:10:19 +0200

    Forgot to add a requires.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.mystic_alchem.ics2java {
     requires commons.io;
+    requires org.jetbrains.annotations;
     exports com.mystic_alchemy.ics2java;
     exports com.mystic_alchemy.ics2java.calendar;
     exports com.mystic_alchemy.ics2java.enums;
