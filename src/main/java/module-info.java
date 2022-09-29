fe7b1bdf2edea23fa0c713cfe0f51dfcd278d8f9
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2022-09-29 15:50:37 +0200

    Added module info due to possible issues.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.mystic_alchem.ics2java {
+    requires commons.io;
+    exports com.mystic_alchemy.ics2java;
+    exports com.mystic_alchemy.ics2java.calendar;
+    exports com.mystic_alchemy.ics2java.enums;
+}
